b1969a31c5678f1f7d596fdcb60aa1c13e16f7dd
Spawn Colen <scolen@va-e5.kzn.21-school.ru>
2020-11-14 11:21:49 +0300

    fix

--- data/data.js
+++ data/data.js
@@ -1,8 +1,8 @@
 const STORE = {
 	pageSettings: {
-		name: "adustin",
+		name: "scolen",
 		colorscheme: "dark",
-		logo: "https://instagram.fppk1-1.fna.fbcdn.net/v/t51.2885-15/e35/115729328_892234564600789_5276454620485538371_n.jpg?_nc_ht=instagram.fppk1-1.fna.fbcdn.net&_nc_cat=101&_nc_ohc=qVlnwzO7C4AAX9FU-Ns&tp=18&oh=895697bb8f0b1aa2ded5bfb8da2d8cd2&oe=5FD504C6"
+		logo: "../image_scolen.png"
 	},
 	links:[
 		{
@@ -18,7 +18,7 @@ const STORE = {
 		{
 			name: "ИНТРА",
 			subtitle: "проекты",
-			link: "https://projects.intra.42.fr/",
+			link: "https://profile.intra.42.fr/",
 			color: "default"
 		},
 		{
